223211db1188bf0bdbe32625cba4961869ffdc68
Kiyoshi Shinya <kshinya@buk.cl>
2026-02-18 23:04:25 -0300

    chore: disable git blame by default

--- .config/nvim/lua/plugins/git-blame.lua
+++ .config/nvim/lua/plugins/git-blame.lua
@@ -6,7 +6,7 @@ return {
     -- If you want to load the plugin at startup, add something like event = "VeryLazy",
     -- or lazy = false. One of both options will work.
     opts = {
-        enabled = true,  -- if you want to enable the plugin
+        enabled = false,  -- if you want to enable the plugin
         message_template = "    <author>, <date> • <summary>", -- template for the blame message, check the Message template section for more options
         date_format = "%r", -- template for the date, check Date format section for more options
         virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -4,7 +4,6 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",         -- required
     "sindrets/diffview.nvim",        -- optional - Diff integration
-
     "ibhagwan/fzf-lua",              -- optional
   },
   cmd = "Neogit",
