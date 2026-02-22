0ccde3f03e55c9adf2d4fb09c4696bd6e9d7469e
Kiyoshi Shinya <kshinya@buk.cl>
2026-02-21 23:03:34 -0300

    feat: change diffview for codediff

---
+++ .config/nvim/lua/plugins/codediff.lua
@@ -0,0 +1,65 @@
+return {
+  "esmuellert/codediff.nvim",
+  cmd = "CodeDiff",
+  opts = {
+    highlights = {
+      line_insert = "DiffAdd",
+      line_delete = "DiffDelete",
+      char_insert = "DiffAddText",
+      char_delete = "DiffDeleteText",
+    },
+    explorer = {
+      position = "bottom",
+    },
+  },
+  config = function(_, opts)
+    require("codediff").setup(opts)
+    vim.api.nvim_create_autocmd("OptionSet", {
+      pattern = "diff",
+      callback = function()
+        vim.wo.wrap = vim.v.option_new == "1"
+      end,
+    })
+  end,
+  keys = {
+    {
+      "<leader>gd",
+      "<cmd>CodeDiff<CR>",
+      desc = "CodeDiff: working tree",
+    },
+    {
+      "<leader>gh",
+      "<cmd>CodeDiff history<CR>",
+      desc = "CodeDiff: branch history",
+    },
+    {
+      "<leader>gH",
+      "<cmd>CodeDiff history HEAD~100 %<CR>",
+      desc = "CodeDiff: file history",
+    },
+    {
+      "<leader>gp",
+      function()
+        local handle = io.popen(
+          "gh pr view --json baseRefOid -q .baseRefOid 2>/dev/null"
+        )
+        if not handle then
+          vim.notify("Failed to run gh", vim.log.levels.ERROR)
+          return
+        end
+
+        local base = handle:read("*a")
+        handle:close()
+
+        base = base:gsub("%s+", "")
+        if base == "" then
+          vim.notify("Not on a PR branch", vim.log.levels.WARN)
+          return
+        end
+
+        vim.cmd("CodeDiff " .. base .. "...")
+      end,
+      desc = "CodeDiff: GitHub PR diff",
+    },
+  },
+}
--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -2,11 +2,14 @@ return {
   "NeogitOrg/neogit",
   lazy = true,
   dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-    "ibhagwan/fzf-lua",              -- optional
+    "nvim-lua/plenary.nvim",
+    "esmuellert/codediff.nvim",
+    "ibhagwan/fzf-lua",
   },
   cmd = "Neogit",
+  opts = {
+    diff_viewer = "codediff",
+  },
   keys = {
     { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
   }
